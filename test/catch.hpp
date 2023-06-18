6ec447d2ce2d68f1c777fa3c69c9a67c59b95c05
sasantu6 <136433050+sasantu6@users.noreply.github.com>
2023-06-18 13:17:57 +0530

    Update catch.hpp

--- test/catch.hpp
+++ test/catch.hpp
@@ -10883,7 +10883,8 @@ namespace Catch {
     bool FatalConditionHandler::isSet = false;
     struct sigaction FatalConditionHandler::oldSigActions[sizeof(signalDefs)/sizeof(SignalDefs)] = {};
     stack_t FatalConditionHandler::oldSigStack = {};
-    char FatalConditionHandler::altStackMem[(std::size_t)sigStackSize] = {};
+     std::size_t stsz = sigStackSize;
+    char FatalConditionHandler::altStackMem[stsz] = {};
 
 } // namespace Catch
 
