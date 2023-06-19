313f21f2e292cc583fd004eeccd7cff6f5126b36
sasantu6 <136433050+sasantu6@users.noreply.github.com>
2023-06-19 15:39:01 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -10,7 +10,7 @@ TEST_CASE("infers the breach according to limits:TOO_HIGH") {
   REQUIRE(inferBreach(32, 20, 30) == TOO_HIGH);
 }
 TEST_CASE("infers the breach according to limits:NORMAL") {
-  REQUIRE(inferBreach(32, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(22, 20, 30) == NORMAL);
 }
 TEST_CASE("classify temperature according to cooling type test1") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH);
