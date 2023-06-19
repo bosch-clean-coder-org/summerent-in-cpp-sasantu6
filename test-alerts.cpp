bf3329a20f1eba9af1c7d60340ba90e3dd8213da
sasantu6 <136433050+sasantu6@users.noreply.github.com>
2023-06-19 16:02:41 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,12 +6,6 @@
 TEST_CASE("infers the breach according to limits UC1") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
-TEST_CASE("infers the breach according to limits UC2") {
-  REQUIRE(inferBreach(32, 20, 30) == TOO_HIGH);
-}
-TEST_CASE("infers the breach according to limits UC3") {
-  REQUIRE(inferBreach(22, 20, 30) == NORMAL);
-}
 TEST_CASE("classify temperature according to cooling type UC1") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH);
 }
@@ -20,4 +14,14 @@ TEST_CASE("classify temperature according to cooling type UC2") {
 }
 TEST_CASE("classify temperature according to cooling type UC3") {
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
+}
+TEST_CASE("checkAndAlert UC1")
+{
+	BatteryCharacter BatteryChar;
+  BatteryChar.coolingType = PASSIVE_COOLING;
+	checkAndAlert(TO_CONTROLLER,BatteryChar)
+	SECTION("Fetch alert")
+	{
+	  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40) == NORMAL);
+	}
 }
